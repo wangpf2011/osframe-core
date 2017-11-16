cfe38bbc147ab95de2cf45a1a413b94a2d18a29b
wangpf <wangpf2011@163.com>
2017-11-16 14:52:48 +0800

    author

--- src/main/java/com/wf/ssm/common/persistence/mybatis/dialect/db/HSQLDialect.java
+++ src/main/java/com/wf/ssm/common/persistence/mybatis/dialect/db/HSQLDialect.java
@@ -11,7 +11,7 @@ import com.wf.ssm.common.persistence.mybatis.dialect.Dialect;
 /**
  * Dialect for HSQLDB
  *
- * @author poplar.yfyang
+ * @author wangpf
  * @version 1.0 2010-10-10 下午12:31
  * @since JDK 1.5
  */
